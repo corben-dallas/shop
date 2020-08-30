9cae6617bcdea2bdf014ce0b73030f070870d5e8
albert_u <albert.u1994@gmail.com>
2020-08-30 10:56:29 +0300

    Updates

---
+++ precache-manifest.c45b5f6511f6a30e51a40a44a238e98f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aa1a1ff48c50df49e1e81c953b126cc",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "3f6120736590b2523681",
+    "url": "/shop/static/css/main.e185357e.chunk.css"
+  },
+  {
+    "revision": "98730f606bcbbe4cc708",
+    "url": "/shop/static/js/2.5f47fdc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shop/static/js/2.5f47fdc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6120736590b2523681",
+    "url": "/shop/static/js/main.91e0aa3c.chunk.js"
+  },
+  {
+    "revision": "34ec67894d57318ba780",
+    "url": "/shop/static/js/runtime-main.ddbc8eb7.js"
+  },
+  {
+    "revision": "9c63a2c92146aa97904b9148bc055ce3",
+    "url": "/shop/static/media/item-black.9c63a2c9.jpg"
+  },
+  {
+    "revision": "8bcbdffead0d5ba6700c1551189af1b8",
+    "url": "/shop/static/media/item-blue.8bcbdffe.jpg"
+  }
+]);
